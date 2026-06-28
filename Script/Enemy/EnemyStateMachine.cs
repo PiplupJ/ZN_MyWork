1801712b908f1066319d03c99cdb50316baea817
Wonseok <99707012+PiplupJ@users.noreply.github.com>
2026-06-28 22:07:20 +0900

    script update

--- Script/Enemy/EnemyStateMachine.cs
+++ Script/Enemy/EnemyStateMachine.cs
@@ -3,25 +3,29 @@ using System;
 
 public class EnemyStateMachine : MonoBehaviour
 {
+    //現在のステート
     protected EnemyState currentEnemyState;
 
-    //1/9追加
+    //1/9追加。シーンマネジャーにボスが倒れたことを知らせる
     public event Action TakenDown;
    
+   //ステート変更
     public void SwitchState(EnemyState newEnemyState)
     {
+        //実行中のステートがあれば終了
         if(currentEnemyState != null)
         {
             currentEnemyState.Exit();
         }
-
+        //ステート変更
         currentEnemyState = newEnemyState;
-
+        //変更を成功したら開始処理
         if(currentEnemyState != null)
         {
             currentEnemyState.Enter();
         }
     }
+    //ステートの更新
     protected void Update()
     {
         if(currentEnemyState!=null)
@@ -30,6 +34,7 @@ public class EnemyStateMachine : MonoBehaviour
         }
     }
     
+    //体力が0になったら、体力クラスによって実行
     public void BattleFinish()
     {
         if(TakenDown!=null)
